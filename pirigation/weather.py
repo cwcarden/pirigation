8b4066b84572898007211bf0ec7d926454acab37
equineranch <charles@charlescarden.com>
2019-05-01 08:16:48 -0500

    revamped weather module

--- pirigation/weather.py
+++ pirigation/weather.py
@@ -7,6 +7,13 @@ api_key = ""
 longitude = "35.2091"
 lattitude = "-101.8890"
 
+"""
+def weather_logger():
+    -Call current weather every 3 hours
+    -Log data to database or JSON file
+
+"""
+
 #Darksky api calls for water weather delay
 def current():
     url = f'https://api.darksky.net/forecast/{api_key}/{longitude},{lattitude}?exclude=minutely,daily,flags'
@@ -35,38 +42,12 @@ def forecast():
         return True # True for engage water
     else:
         return False
-''' history function has five api calls that check for rain from past hour,
-past 3 hr, past 6 hr, past 12 hr, and past 24 hours to determine if any rain has occurred.'''
+"""
 def history():
-    past_day = timestamp - 86400
-    twelve_hour = timestamp - 42300
-    six_hour = timestamp - 21150
-    three_hour = timestamp - 10575
-    one_hour = timestamp - 3525
-    url_past_day = f'https://api.darksky.net/forecast/{api_key}/{longitude},{lattitude},{past_day}'
-    url_past_twelve = f'https://api.darksky.net/forecast/{api_key}/{longitude},{lattitude},{twelve_hour}'
-    url_past_six = f'https://api.darksky.net/forecast/{api_key}/{longitude},{lattitude},{six_hour}'
-    url_past_three = f'https://api.darksky.net/forecast/{api_key}/{longitude},{lattitude},{three_hour}'
-    url_past_hr = f'https://api.darksky.net/forecast/{api_key}/{longitude},{lattitude},{one_hour}'
-    res_day = requests.get(url_past_day)
-    res_twelve = requests.get(url_past_twelve)
-    res_six = requests.get(url_past_six)
-    res_three = requests.get(url_past_three)
-    res_hr = requests.get(url_past_hr)
-    weather_data_24 = res_day.json()
-    weather_data_12 = res_twelve.json()
-    weather_data_6 = res_six.json()
-    weather_data_3 = res_three.json()
-    weather_data_hr = res_hr.json()
-    pastcast_24 = weather_data_24['hourly']['data'][0]['icon']
-    pastcast_12 = weather_data_12['hourly']['data'][0]['icon']
-    pastcast_6 = weather_data_6['hourly']['data'][0]['icon']
-    pastcast_3 = weather_data_3['hourly']['data'][0]['icon']
-    pastcast_hr = weather_data_hr['hourly']['data'][0]['icon']
-    if pastcast_24 == 'rain' or pastcast_12 == 'rain' or pastcast_6 == 'rain' or pastcast_3 == 'rain' or pastcast_hr == 'rain':
-        return True
-    else:
-        return False
+    -Parses weather_logger data in either json or db
+    -checks if rain event was in past ? hours of current timestamp
+"""
+
 
 def all_weather():
     if forecast() or current() == True or history() == True:
