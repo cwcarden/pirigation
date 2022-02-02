bd6f2a911f3914e374e1e9d1b6992ad32d78e53b
charles w carden <charles@charlescarden.com>
2022-02-01 22:52:35 -0600

    Update start.py

--- start.py
+++ start.py
@@ -1,8 +1,5 @@
+#Use this module to start entire application
 from pirigation import app
 
 if __name__== '__main__':
-    app.run(debug=True,port=4000)
-
-    """
-    Use this module to start entire application
-    """
+    app.run(debug=True,port=4000)   
